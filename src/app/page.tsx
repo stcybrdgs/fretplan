25ef5f994f343444e97c118147e59d109c6e7e58
Stacy Bridges <stcybrdgs@gmail.com>
2025-07-10 01:15:40 -0500

    fix: escape HTML entities for production build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -266,7 +266,7 @@ export default function Home() {
                         className='w-4 h-4 text-purple-600 border-gray-300 rounded focus:ring-purple-500'
                       />
                       <span className='text-gray-900 dark:text-white'>
-                        Apply to "Autumn Leaves" in Bb
+                        Apply to &quot;Autumn Leaves&quot; in Bb
                       </span>
                     </div>
 
@@ -294,7 +294,7 @@ export default function Home() {
                   <div className='flex items-center space-x-3'>
                     <span className='w-3 h-3 bg-purple-600 rounded-full'></span>
                     <h3 className='text-lg font-medium text-gray-900 dark:text-white'>
-                      Autumn Leaves - Bb & G Major
+                      Autumn Leaves - Bb &amp; G Major
                     </h3>
                   </div>
                   <ChevronRight className='w-5 h-5 text-gray-500 dark:text-gray-400' />
@@ -315,4 +315,3 @@ export default function Home() {
     </div>
   )
 }
-
